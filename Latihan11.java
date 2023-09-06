c2c4a44973c9fba34e20df9751a3af7ef363f1e5
flaxelyn <fallujahrc@gmail.com>
2023-09-06 18:41:58 +0700

    coba push folder

---
+++ Latihan11.java
@@ -0,0 +1,12 @@
+/**
+ * Latihan11
+ */
+public class Latihan11 {
+
+    public static void main(String[] args) {
+        System.out.println("Haloo!"); //ln -line, insert new line at the end of out
+        System.out.print("Nama saya Fallujah Ramadi Camshah");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA JOSS!");
+        
+    }
+}
