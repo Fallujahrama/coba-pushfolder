ce2e2f06a48144a0485aac47add63984d97fb3e4
flaxelyn <fallujahrc@gmail.com>
2023-09-06 18:38:42 +0700

    coba push folder

---
+++ Tugas11.java
@@ -0,0 +1,10 @@
+public class Tugas11 {
+    public static void main(String[] args ){
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama            : Fallujah Ramadi Camshah");
+        System.out.println("Absen           : 11");
+        System.out.println("Kelas           : 1-D");
+        System.out.println("Prodi           : Sistem Informasi Bisnis");
+        System.out.println("Jurusan         : Teknologi Informasi");
+    }
+}
